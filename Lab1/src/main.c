5dfd570ab3f50eae4c97e04907be1e8a04d27bfd
EgorS <egor.sima.2003@gmail.com>
2021-03-08 12:13:56 +0300

    Lab1

--- Lab1/src/main.c
+++ Lab1/src/main.c
@@ -6,52 +6,22 @@ int main(void) {
 	unsigned int m;
 	
 	int correctInput = scanf("%u", &m);
-	printf("\n%d\n%d\n", m, correctInput);
 	if (correctInput == 1 && m <= 2147483647) {
 		int change = 0;
-
 		while (number <= m) {
 			printf("DEC: %11lu   ", number);
 			printf("BIN: ");
 			toBin(number);
 			printf("\n");
-
-
 			if (number > 1073741824) break;
-
 			number <<= 2;
-
 			if (change) {
 				number |= 3;
 				change--;
 				continue;
 			}
-
 			change++;
 		}
 	}
-
-	int change = 0;
-
-	while (number <= m) {
-		printf("DEC: %11lu   ", number);
-		printf("BIN: ");
-		toBin(number);
-		printf("\n");
-
-
-		if (number > 1073741824) break;
-
-		number <<= 2;
-
-		if (change) {
-			number |= 3;
-			change--;
-			continue;
-		}
-
-		change++;
-	}
-	scanf("%d", &m);
 	return 0;
 }
