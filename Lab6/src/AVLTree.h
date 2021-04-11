dfefaf19f3e6dc67640d1c6299d2f04cf3b24729
EgorS <egor.sima.2003@gmail.com>
2021-04-11 11:19:08 +0300

    Lab6

---
+++ Lab6/src/AVLTree.h
@@ -0,0 +1,91 @@
+#pragma once 
+
+#include <cassert>
+
+
+class AVLTree {
+private:
+	struct Node
+	{
+		int key;
+		int height;
+		Node* left;
+		Node* right;
+		Node(int k) : key(k), height(1), left(nullptr), right(nullptr) {}
+	};
+
+	Node* root;
+
+	int height(const Node* obj) const {
+		return (obj ? obj->height : 0);
+	}
+
+	int balanceFactor(const Node& obj) const {
+		return (height(obj.right) - height(obj.left));
+	}
+
+	void fixHeight(Node& obj) const {
+		int h1 = height(obj.left);
+		int h2 = height(obj.right);
+		obj.height = (h1 > h2 ? h1 : h2) + 1;
+	}
+
+	Node* rotateRight(Node& obj) {
+		Node* ptr = obj.left;
+		obj.left = ptr->right;
+		ptr->right = &obj;
+		fixHeight(obj);
+		fixHeight(*ptr);
+		return ptr;
+	}
+
+	Node* rotateLeft(Node& obj) {
+		Node* ptr = obj.right;
+		obj.right = ptr->left;
+		ptr->left = &obj;
+		fixHeight(obj);
+		fixHeight(*ptr);
+		return ptr;
+	}
+
+	Node* balance(Node* obj) {
+		fixHeight(*obj);
+		if (balanceFactor(*obj) == 2) {
+			if (balanceFactor(*obj->right) < 0) obj->right = rotateRight(*obj->right);
+			return rotateLeft(*obj);
+		}
+		else if (balanceFactor(*obj) == -2) {
+			if (balanceFactor(*obj->left) > 0) obj->left = rotateLeft(*obj->left);
+			return rotateRight(*obj);
+		}
+		return obj;
+	}
+
+	Node* _insert(Node* obj, int k) {
+		if (!obj) return new Node(k);
+		if (k < obj->key) obj->left = _insert(obj->left, k);
+		else obj->right = _insert(obj->right, k);
+		return balance(obj);
+	}
+
+	void _checkAllNodesBF(const Node* obj) {
+		if (!obj) return;
+		_checkAllNodesBF(obj->left);
+		int b = balanceFactor(*obj);
+		assert(b < 2 && b > -2);
+		_checkAllNodesBF(obj->right);
+	}
+
+public:
+	AVLTree() : root(nullptr) {}
+
+
+	void insert(int k) {
+		root = _insert(root, k);
+	}
+
+	void checkAllNodesBF() {
+		_checkAllNodesBF(root);
+	}
+
+};
---
+++ Lab6/src/MainAndTests.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <cassert>
+#include "AVLTree.h"
+
+int main() {
+	std::ifstream fin;
+	fin.open("input.txt");
+	assert(fin);
+
+	AVLTree tr;
+	while (!fin.eof()) {
+		int temp;
+		fin >> temp;
+		tr.insert(temp);
+	}
+
+	tr.checkAllNodesBF();
+
+	return 0;
+}
